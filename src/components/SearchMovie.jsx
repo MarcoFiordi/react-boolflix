ba5949d817da8282256f0a2c0dc9cf6b152a1a84
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-19 00:36:39 +0200

    aggiunte le bandiere della lingua dei film

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -4,6 +4,31 @@ function SearchMovie() {
     const [searchText, setSearchText] = useState("");
     const [movies, setMovies] = useState([]);
 
+    function getFlag(language){
+        const flags = {
+            en: 'gb',
+            it: 'it',
+            fr: 'fr',
+            es: 'es',
+            de: 'de',
+            ja: 'jp',
+            ko: 'kr',
+            zh: 'cn',
+        };
+        const countryCode = flags[language];
+
+        if (!countryCode){
+            return language;
+        }
+        
+        return(
+            <img 
+            src={`https://flagcdn.com/24x18/${countryCode}.png`} 
+            alt={language} 
+            />
+        );
+    }
+
     function handleSearch() {
         const token = import.meta.env.VITE_TMDB_READ_ACCESS_TOKEN;
         const url = `https://api.themoviedb.org/3/search/movie?query=${searchText}&language=it-IT`;
@@ -47,10 +72,10 @@ function SearchMovie() {
 
             {movies.map((movie)=> (
                 <div key={movie.id}>
-                    <h3>{movie.title}</h3>
-                    <p>Titolo originale:{movie.original_title}</p>
-                    <p>Lingua:{movie.original_language}</p>
-                    <p>Voto:{movie.vote_average}</p>
+                    <h3> {movie.title}</h3>
+                    <p>Titolo originale: {movie.original_title}</p>
+                    <p>Lingua: {getFlag(movie.original_language)}</p>
+                    <p>Voto: {movie.vote_average}</p>
                 </div>
             ))}
         </div>
